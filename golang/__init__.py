1654640f0c5f6bbb8e719abfa374f38d4130e7f7
menduo <shimenduo@gmail.com>
2024-05-01 14:23:53 +0800

    use explicit import

--- golang/__init__.py
+++ golang/__init__.py
@@ -1,4 +1,9 @@
 # flake8: noqa
-from golang.__internal__.env import *  # noq: F403,F401
+from golang.__internal__.env import (
+    Env,
+)
 # flake8: noqa
-from golang.__internal__.run import *  # noqa: F403,F401
+from golang.__internal__.run import (
+    run_code,
+    run_file,
+)
